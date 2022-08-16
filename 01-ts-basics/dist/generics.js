2ece8a87637991a8dbc467659373f3039a7e2df1
Sumit K <synergy2411@outlook.com>
2022-08-16 16:31:20 +0530

    adv generics

--- 01-ts-basics/dist/generics.js
+++ 01-ts-basics/dist/generics.js
@@ -1,9 +1,55 @@
 "use strict";
-function addAtBeginning(num, arr) {
-    return [num, ...arr];
-}
-const numberArray = addAtBeginning(10, [9, 8, 7, 6]);
-const stringArray = addAtBeginning("A", ["B", "C", "D"]);
+let resourceTwo = {
+    myResources: {
+        resourceName: "",
+        resourceId: 0
+    }
+};
+// interface Resource<T,U>{
+//     resourceName : T,
+//     resournceId : U
+// }
+// let resourceOne : Resource<string, number> = {
+//     resourceName: "Employee Admin",
+//     resournceId: 1001
+// }
+// class Person<T extends PersonType>{
+//     constructor(private obj: T) {
+//     }
+//     getDetails() {
+//         return this.obj;
+//     }
+// }
+// type PersonType = {
+//     firstName: string,
+//     lastName: string,
+//     age: number
+// }
+// let objThree: PersonType = {
+//     firstName: "",
+//     lastName: "",
+//     age: 32
+// }
+// let myObj = new Person<{
+//     firstName: string, lastName: string, age: number, avatar: string, city: string
+// }>({ firstName: "Foo", lastName: "Bar", age: 32, avatar: "", city: "" })
+// let myObjTwo = new Person<{
+//     avatar: string, city: string
+// } & PersonType>({ avatar: "", city: "", ...objThree })
+// console.log(myObj.getDetails())
+// class Courses<T, U>{
+//     constructor(private courseId : T, private courseName : U){}
+//     getCourseId() : string {
+//         return "Course ID " + this.courseId;
+//     }
+// }
+// let reactCourse = new Courses<string, string>("101", "React for Beginners")
+// let angularCourse = new Courses<number, string>(102, "Mastering Angular");
+// function addAtBeginning<T>(num: T, arr: Array<T>): Array<T> {
+//     return [num, ...arr]
+// }
+// const numberArray = addAtBeginning<number>(10, [9, 8, 7, 6]);
+// const stringArray = addAtBeginning<string>("A", ["B", "C", "D"])
 // stringArray[0].
 // numberArray[0].
 // const numArray = addAtBeginning(4, [3,4,6,7])
--- 01-ts-basics/src/generics.ts
+++ 01-ts-basics/src/generics.ts
@@ -1,10 +1,96 @@
-function addAtBeginning<T>(num: T, arr: Array<T>): Array<T> {
-    return [num, ...arr]
+type ResourceType = { resourceName: string }
+
+interface Resource<T extends { resourceName: string }> {
+    myResources: T
 }
 
-const numberArray = addAtBeginning<number>(10, [9, 8, 7, 6]);
+let resourceTwo: Resource<{ resourceName: string, resourceId: number }> = {
+    myResources: {
+        resourceName: "",
+        resourceId: 0
+    }
+}
 
-const stringArray = addAtBeginning<string>("A", ["B", "C", "D"])
+
+// interface Resource<T,U>{
+//     resourceName : T,
+//     resournceId : U
+// }
+
+// let resourceOne : Resource<string, number> = {
+//     resourceName: "Employee Admin",
+//     resournceId: 1001
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Person<T extends PersonType>{
+//     constructor(private obj: T) {
+//     }
+//     getDetails() {
+//         return this.obj;
+//     }
+// }
+
+// type PersonType = {
+//     firstName: string,
+//     lastName: string,
+//     age: number
+// }
+
+// let objThree: PersonType = {
+//     firstName: "",
+//     lastName: "",
+//     age: 32
+// }
+
+// let myObj = new Person<{
+//     firstName: string, lastName: string, age: number, avatar: string, city: string
+// }>({ firstName: "Foo", lastName: "Bar", age: 32, avatar: "", city: "" })
+
+// let myObjTwo = new Person<{
+//     avatar: string, city: string
+// } & PersonType>({ avatar: "", city: "", ...objThree })
+
+
+// console.log(myObj.getDetails())
+
+
+
+
+
+// class Courses<T, U>{
+//     constructor(private courseId : T, private courseName : U){}
+//     getCourseId() : string {
+//         return "Course ID " + this.courseId;
+//     }
+// }
+
+// let reactCourse = new Courses<string, string>("101", "React for Beginners")
+
+// let angularCourse = new Courses<number, string>(102, "Mastering Angular");
+
+// function addAtBeginning<T>(num: T, arr: Array<T>): Array<T> {
+//     return [num, ...arr]
+// }
+
+// const numberArray = addAtBeginning<number>(10, [9, 8, 7, 6]);
+
+// const stringArray = addAtBeginning<string>("A", ["B", "C", "D"])
 
 // stringArray[0].
 
