1a630a2e1c051c6a2af45d91f37d29a34d233c11
Sumit K <synergy2411@outlook.com>
2022-08-17 17:17:24 +0530

    parent -child comm

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { ParentComponent } from './components/playground/parent/parent.component';
+import { ChildComponent } from './components/playground/parent/child/child.component';
 
 @NgModule({
   declarations: [         // Component, Directive, Pipe
-    AppComponent, UsersComponent, UserDetailComponent
+    AppComponent, UsersComponent, UserDetailComponent, ParentComponent, ChildComponent
   ],
   imports: [
     BrowserModule,         // Module
