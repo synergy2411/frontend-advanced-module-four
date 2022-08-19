10bd3222cb3b57bc755faa5c0fcaa7e63df601b9
Sumit K <synergy2411@outlook.com>
2022-08-19 14:05:32 +0530

    form validation

--- expense-app/src/components/Auth/Login/Login.js
+++ expense-app/src/components/Auth/Login/Login.js
@@ -5,25 +5,62 @@ const Login = () => {
     const inputEmailRef = useRef();
 
     const [password, setPassword] = useState('');
+    const [emailIsValid, setEmailIsValid] = useState(false);
+    const [emailIsFocused, setEmailIsFocused] = useState(false);
+
+    const [passwordIsValid, setPasswordIsValid] = useState(false);
+    const [passwordIsFocused, setPasswordIsFocused] = useState(false);
 
     const passwordChangeHandler = event => setPassword(event.target.value)
 
+    const emailBlurHandler = () => {
+        if (inputEmailRef.current.value.trim() === '') {
+            setEmailIsValid(false)
+        } else {
+            setEmailIsValid(true)
+        }
+    }
+    const emailFocusHandler = () => {
+        setEmailIsFocused(true)
+    }
     const submitHandler = event => {
         event.preventDefault();
         console.log("Email : ", inputEmailRef.current.value)
         console.log("Password : ", password);
     }
+
+    const passwordBlurHandler = () => {
+        password.trim() === '' ? setPasswordIsValid(false) : setPasswordIsValid(true)
+    }
+
+    const passwordFocusHandler = () => setPasswordIsFocused(true)
+
+    let emailValidity = emailIsFocused && !emailIsValid;
+    let passwordValidity = passwordIsFocused && !passwordIsValid;
+
+    let formValidity = emailValidity && passwordValidity;
+
     return (
         <form onSubmit={submitHandler}>
             <label>Email :</label>
-            <input type="email" name="email" id="email" ref={inputEmailRef} />
+            <input type="email"
+                name="email"
+                id="email"
+                ref={inputEmailRef}
+                onBlur={emailBlurHandler}
+                onFocus={emailFocusHandler} />
+            {emailValidity && <p>Email is mandatory field</p>}
             <br />
             <label>Password : </label>
             <input type="password" name="password"
                 value={password}
-                onChange={passwordChangeHandler} />
+                onChange={passwordChangeHandler}
+                onBlur={passwordBlurHandler}
+                onFocus={passwordFocusHandler} />
+            {passwordValidity && <p>Password is adatory field.</p>}
             <br />
-            <button type="submit">Login</button>
+            {formValidity ? 'true' : 'false'}
+            <button type="submit" disabled={formValidity}>Login</button>
         </form>
     )
 }
