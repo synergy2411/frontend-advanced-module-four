162023950c8ec2119196d8d684bfcb580c8d1e82
Sumit K <synergy2411@outlook.com>
2022-08-24 16:57:45 +0530

    assignment added

--- expense-app/src/store/index.js
+++ expense-app/src/store/index.js
@@ -3,11 +3,16 @@ import { logger } from './middlewares/logger';
 // import rootReducer from './reducers';
 import counterReducer from './reducers/counter.reducer';
 import resultReducer from './reducers/result.reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // const store = createStore(rootReducer)
 
 const store = createStore(combineReducers({
     ctr: counterReducer,
     res: resultReducer
-}), applyMiddleware(logger))
+}), composeWithDevTools(applyMiddleware(logger)))
+
 export default store;
+
+
+// npm i redux-devtools-extension -D
